9d5a9dd7de6fabba6df509b64bab0c27230f5846
OsherKoren <56833942+OsherKoren@users.noreply.github.com>
2021-06-01 12:34:02 +0300

    Update getStockData.py

--- getStockData.py
+++ getStockData.py
@@ -5,14 +5,14 @@ from projectLogger import Mylogger
 
 def get_stock_data(stocks_df, stock_index):
     flowLogger = Mylogger('flow_logger').log()
-    flowLogger.info(f' --------------------------------- Flow Prosses Started------------------------------------')
+    flowLogger.info(f' --------------------------------- Flow Process Started------------------------------------')
 
     stockFlow = pd.read_csv('stock_flow.csv', sep="-").set_index('steps')['functions'].to_dict()
     stock = Stock(stocks_df, stock_index)
     for step in stockFlow:
         eval(stockFlow[step])
 
-    flowLogger.info(f' ---------------------------------  Prosses Completed ------------------------------------')
+    flowLogger.info(f' ---------------------------------  Process Completed ------------------------------------')
 
 def start_flow(df):
     flowLogger = Mylogger('flow_logger').log()
@@ -20,7 +20,7 @@ def start_flow(df):
         for i in range(30, len(df.index)):
             get_stock_data(df, df.index[i])
 
-        flowLogger.info(f' ---------------------------------  Prosses Completed ------------------------------------')
+        flowLogger.info(f' ---------------------------------  Process Completed ------------------------------------')
 
     except Exception as e:
         flowLogger.error(f'xxxxxxxxxxxxxxxxxxxxxxxxxxx Flow Program Stopped xxxxxxxxxxxxxxxxxxxxxxxxxxx {repr(e)}')
